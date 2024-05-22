c0afe7d730c9c167118c0158fa664481a14a9853
Mark Story <mark@mark-story.com>
2024-05-22 12:36:42 -0400

    Switch to using a service_fn
    Using the Service struct was making it hard to read the body
    as the body call() is not async, and reading the body requires async.

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,16 @@ use std::net::SocketAddr;
 use std::path::Path;
 use std::sync::Arc;
 
+use hyper::body::Incoming;
 use hyper::server::conn::http1;
+use hyper::service::service_fn;
+use hyper::Request;
 use hyper_util::rt::TokioIo;
 use tokio::net::TcpListener;
 
 mod config;
 mod dsn;
+mod request;
 mod service;
 
 #[tokio::main]
@@ -29,19 +33,18 @@ async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
     // Bind to a port
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
     let listener = TcpListener::bind(addr).await?;
-    let handler = service::MirrorService {
-        keymap: Arc::new(keymap),
-    };
+    let arcmap = Arc::new(keymap);
 
     loop {
         let (stream, _) = listener.accept().await?;
         let io = TokioIo::new(stream);
-        let handler_clone = handler.clone();
+        let arcmap_loop = arcmap.clone();
 
         tokio::task::spawn(async move {
-            // Attach the mirror service function
             if let Err(err) = http1::Builder::new()
-                .serve_connection(io, handler_clone)
+                .serve_connection(io, service_fn(move |req: Request<Incoming>| {
+                    service::handle_request(req, arcmap_loop.clone())
+                }))
                 .await
             {
                 eprintln!("Error serving connection: {:?}", err);
@@ -49,3 +52,5 @@ async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
         });
     }
 }
+
+
--- src/service.rs
+++ src/service.rs
@@ -1,50 +1,88 @@
 use std::{collections::HashMap, sync::Arc};
-use std::future::Future;
-use std::pin::Pin;
 
-use http_body_util::Full;
+use http_body_util::{BodyExt, Full};
 use hyper::body::{Bytes, Incoming};
 use hyper::{Method, StatusCode};
-use hyper::{service::Service, Request, Response};
+use hyper::{Request, Response};
 
 use crate::dsn;
+use crate::request;
 
-#[derive(Debug, Clone)]
-pub struct MirrorService {
-    pub keymap: Arc<HashMap<String, Vec<dsn::Dsn>>>,
-}
+type GenericError = Box<dyn std::error::Error + Send + Sync>;
+type Result<T> = std::result::Result<T, GenericError>;
+type BoxBody = http_body_util::combinators::BoxBody<Bytes, hyper::Error>;
 
-impl Service<Request<Incoming>> for MirrorService {
-    type Response = Response<Full<Bytes>>;
-    type Error = hyper::Error;
-    type Future = Pin<Box<dyn Future<Output = Result<Self::Response, Self::Error>> + Send>>;
+pub async fn handle_request(
+    req: Request<Incoming>, keymap: Arc<HashMap<String, dsn::DsnKeyRing>>
+) -> Result<Response<BoxBody>> {
+    let method = req.method();
+    let uri = req.uri();
+    let headers = req.headers();
 
-    fn call(&self, req: Request<Incoming>) -> Self::Future {
-        // All store/envelope requests are POST
-        if req.method() != Method::POST {
-            let mut res = Response::new(Full::new(Bytes::from("Method not allowed")));
-            *res.status_mut() = StatusCode::METHOD_NOT_ALLOWED;
-            return Box::pin(async { Ok(res) })
-        }
-        // Find DSN public key in request
-        let found_dsn = dsn::from_request(req.uri(), req.headers());
-        if found_dsn.is_none() {
-            let res = Response::builder()
-                .status(StatusCode::BAD_REQUEST)
-                .body(Full::new(Bytes::from("No DSN found")))
-                .unwrap();
-            return Box::pin(async { Ok(res) })
-        }
-        // Match the public key with registered keys
-
-        // If a DSN cannot be found -> empty response
-
-        // Match the inbound DSN to one we have in our configuration
-        // If a DSN cannot be matched -> empty response
-
-        // Async send a modified request to each outbound DSN
-
-        let res = Response::new(Full::new(Bytes::from("Hello world")));
-        return Box::pin(async { Ok(res) })
+    // All store/envelope requests are POST
+    if method != Method::POST {
+        let res = Response::builder()
+            .status(StatusCode::METHOD_NOT_ALLOWED)
+            .body(full("Method not allowed"))
+            .unwrap();
+        return Ok(res);
     }
+    // Find DSN public key in request
+    let found_dsn = dsn::from_request(uri, headers);
+    if found_dsn.is_none() {
+        return Ok(bad_request_response());
+    }
+    // Match the public key with registered keys
+    let public_key = found_dsn.unwrap();
+    let keyring = match keymap.get(&public_key) {
+        Some(v) => v,
+        // If a DSN cannot be found -> empty response
+        None => return Ok(bad_request_response())
+    };
+    for outbound_dsn in keyring.outbound.iter() {
+        let _outbound_request = request::make_outbound_request(headers, &keyring.inbound, &outbound_dsn);
+    }
+    let whole_body = req.collect().await?.to_bytes();
+    println!("body!! {:?}", whole_body);
+
+    let res_body = full("hello");
+    Ok(Response::new(res_body))
 }
+
+
+fn bad_request_response() -> Response<BoxBody> {
+    return Response::builder()
+        .status(StatusCode::BAD_REQUEST)
+        .body(full("No DSN found"))
+        .unwrap();
+}
+
+fn full<T: Into<Bytes>>(chunk: T) -> BoxBody {
+    Full::new(chunk.into())
+        .map_err(|never| match never {})
+        .boxed()
+}
+
+/*
+/// Send a request to its destination async
+async fn send_request(req: Request<()>) -> Result<Response<BoxBody>> {
+    let host = req.uri().host().expect("request uri has no host");
+    let port = req.uri().port_u16().unwrap_or(80);
+
+    /*
+    let stream = TcpStream::connect((host, port)).await?;
+    let io = TokioIo::new(stream);
+    let (mut sender, conn) = hyper::client::conn::http1::handshake(io)
+        .await?;
+
+    tokio::task::spawn(async move {
+        if let Err(err) = conn.await {
+            println!("Connection failed: {:?}", err);
+        }
+    });
+
+    let resp = sender.send_request(req).await?;
+    */
+    Ok(Response::builder().body(full("works?")).unwrap())
+}
+*/
